c684a77ab66fceb74b1a74345af090f001b5a865
Yihun <yankure01@gmail.com>
2024-02-20 00:46:13 +0300

    fdb-01

---
+++ lib/data/models/news_model.dart
@@ -0,0 +1,13 @@
+class News {
+  final String title;
+  final String description;
+  final String url;
+  final String imageUrl;
+
+  News({
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.imageUrl,
+  });
+}
